94bad91fe646944c662a191bc488b762d973e6f2
vadim-project <vadim.rogozhnikov2002@gmail.com>
2023-04-28 20:19:14 +0300

    feat: create keyboard on html and click event

--- index.js
+++ index.js
@@ -13,35 +13,96 @@ const keyBoardWrapper = document.createElement('div');
 keyBoardWrapper.classList = 'keyboard__wrapper';
 container.append(keyBoardWrapper);
 
+const firstRowWrapper = document.createElement('div');
+firstRowWrapper.classList = 'firstRowWrapper';
+const secondRowWrapper = document.createElement('div');
+secondRowWrapper.classList = 'secondRowWrapper';
+const thirdRowWrapper = document.createElement('div');
+thirdRowWrapper.classList = 'thirdRowWrapper';
+const fourRowWrapper = document.createElement('div');
+fourRowWrapper.classList = 'fourRowWrapper';
+const fiftyRowWrapper = document.createElement('div');
+fiftyRowWrapper.classList = 'fiftyRowWrapper';
+
+keyBoardWrapper.append(firstRowWrapper);
+keyBoardWrapper.append(secondRowWrapper);
+keyBoardWrapper.append(thirdRowWrapper);
+keyBoardWrapper.append(fourRowWrapper);
+keyBoardWrapper.append(fiftyRowWrapper);
+
 // create button
 
 const firstRow = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'BACKSPACE'];
 const firstRowAltContent = [`~`, '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+'];
+const secondRow = ['TAB', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\'];
+const thirdRow = ['CAPS LOCK', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", 'ENTER'];
+const fourRow = ['SHIFT', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 'SHIFT'];
+const fiftyRow = ['CTRL', 'WIN', 'L ALT', 'SPACE', 'R ALT', '\\', 'CTRL'];
+const allKeys = [...firstRow, ...secondRow, ...thirdRow, ...fourRow, ...fiftyRow];
+console.log(firstRow.length);
+console.log(secondRow.length);
+console.log(thirdRow.length);
+console.log(fourRow.length);
+console.log(fiftyRow.length);
+
+
 
 window.addEventListener('load', () => {
     createButton();
 })
 
 function createButton() {
-    for (let i = 0; i < firstRow.length; i++) {
+    for (let i = 0; i < allKeys.length; i++) {
         const CreateKeys = document.createElement('button');
-        CreateKeys.classList = 'keys';
-        keyBoardWrapper.appendChild(CreateKeys);
+            CreateKeys.classList = 'keys';
+            if (i < 14) {
+                firstRowWrapper.appendChild(CreateKeys);
+            }
+            if (i < 28 && i >= 14) {
+                secondRowWrapper.appendChild(CreateKeys);
+            }
+            if(i < 41 && i >= 28) {
+                thirdRowWrapper.appendChild(CreateKeys);
+            }
+            if(i < 53 && i >= 41) {
+                fourRowWrapper.appendChild(CreateKeys);
+            }
+            if(i <= 60 && i >= 53) {
+                fiftyRowWrapper.appendChild(CreateKeys);
+            }
     }
 
     const keys = document.querySelectorAll('.keys');
-
-    for (let j = 0; j < firstRow.length; j++) {
-        keys[j].textContent = firstRow[j];
-    }
-
-    for (let x = 0; x < firstRowAltContent.length; x++) {
-        const altKeyContent = document.createElement('span');
-        altKeyContent.classList = 'altContent';
-        altKeyContent.textContent = firstRowAltContent[x];
-        keys[x].appendChild(altKeyContent);
+    
+    for (let j = 0; j < allKeys.length; j++) {
+        keys[j].textContent = allKeys[j];
     }
 
     keys[0].id = 'tilda';
     keys[13].id = 'backspace';
+    keys[14].id = 'tab';
+    keys[28].id = 'caps-lock';
+    keys[40].id = 'enter';
+    keys[41].id = 'l-shift';
+    keys[52].id = 'r-shift';
+    keys[53].id = 'l-ctrl';
+    keys[54].id = 'win';
+    keys[55].id = 'l-alt';
+    keys[56].id = 'space';
+    keys[57].id = 'r-alt';
+    keys[59].id = 'r-ctrl'; 
 }
+
+
+window.addEventListener('keydown', (event) => {
+    let keys = document.querySelectorAll(".keys");
+    let key = Array.from(keys).filter(key => key.textContent.replaceAll(' ', '').toUpperCase() === event.key.replaceAll(' ', '').toUpperCase())[0];
+    key.classList.add('active');
+})
+
+window.addEventListener('keyup', (event) => {
+    let keys = document.querySelectorAll(".keys");
+    let key = Array.from(keys).filter(key => key.textContent.replaceAll(' ', '').toUpperCase() === event.key.replaceAll(' ', '').toUpperCase())[0];
+    key.classList.remove('active');
+})
+
