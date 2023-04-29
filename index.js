4a1c69bee7c62fac51b5339f341724b188520af4
vadim-project <vadim.rogozhnikov2002@gmail.com>
2023-04-29 18:45:51 +0300

    feat: add feature for buttons.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const container = document.createElement('div');
 container.classList = 'wrapper';
 document.body.append(container);
 
-const input = document.createElement('input');
+const input = document.createElement('textarea');
 input.classList = 'text-area'
 input.type = 'text';
 container.append(input);
@@ -31,23 +31,57 @@ keyBoardWrapper.append(fourRowWrapper);
 keyBoardWrapper.append(fiftyRowWrapper);
 
 // create button
-
 const firstRow = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'BACKSPACE'];
-const firstRowAltContent = [`~`, '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+'];
+const firstRowRu = ['ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'BACKSPACE'];
+
+const firstRowAltContent = [`~`, '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', 'BACKSPACE'];
+const firstRowRuAltContent = ['Ё', '!', '"', '№', ';', '%', ':', '?', '*', '(', ')', '_', '+', 'BACKSPACE'];
+
 const secondRow = ['TAB', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\'];
+const secondRowRu = ['TAB', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'o', 'з', 'х', 'ъ', '\\'];
+
+const secondRowAltContent = ['TAB', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '{', '}', '|'];
+const secondRowRuAltContent = ['TAB', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'o', 'з', 'х', 'ъ', '/'];
+
+
 const thirdRow = ['CAPS LOCK', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", 'ENTER'];
+const thirdRowRu = ['CAPS LOCK', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'ENTER'];
+
+const thirdRowAltContent = ['CAPS LOCK', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ':', '"', 'ENTER'];
+
 const fourRow = ['SHIFT', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 'SHIFT'];
+const fourRowRu = ['SHIFT', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', 'SHIFT'];
+
+const fourRowRuAltContent = ['SHIFT', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', ',', 'SHIFT'];
+
+
 const fiftyRow = ['CTRL', 'WIN', 'L ALT', 'SPACE', 'R ALT', '\\', 'CTRL'];
+
+const fiftyRowAltContent = ['CTRL', 'WIN', 'L ALT', 'SPACE', 'R ALT', '|', 'CTRL'];
+
 const allKeys = [...firstRow, ...secondRow, ...thirdRow, ...fourRow, ...fiftyRow];
+const allKeysRu = [...firstRowRu, ...secondRowRu, ...thirdRowRu, ...fourRowRu, ...fiftyRow];
+const allKeysRuAltContent = [...firstRowRuAltContent, ...secondRowRuAltContent, ...thirdRowRu, ...fourRowRuAltContent, ...fiftyRowAltContent];
+
+let keys = {};
+
 
 window.addEventListener('load', () => {
-    createButton();
+    let language = localStorage.getItem('lang') ? localStorage.getItem('lang') : 'ru';
+    if (language === 'en') createButton(allKeys);
+    else createButton(allKeysRu);
 })
 
-function createButton() {
-    for (let i = 0; i < allKeys.length; i++) {
+function createButton(arr) {
+    firstRowWrapper.innerHTML = '';
+    secondRowWrapper.innerHTML = '';
+    thirdRowWrapper.innerHTML = '';
+    fourRowWrapper.innerHTML = '';
+    fiftyRowWrapper.innerHTML = '';
+    for (let i = 0; i < arr.length; i++) {
         const CreateKeys = document.createElement('button');
-        CreateKeys.classList = 'keys';
+        CreateKeys.classList.add('keys');
+        if (arr[i].length <= 1) CreateKeys.classList.add('char');
         if (i < 14) {
             firstRowWrapper.appendChild(CreateKeys);
         }
@@ -65,24 +99,126 @@ function createButton() {
         }
     }
 
-    const keys = document.querySelectorAll('.keys');
+    keys = document.querySelectorAll('.keys');
 
-    for (let j = 0; j < allKeys.length; j++) {
-        keys[j].textContent = allKeys[j];
+    for (let j = 0; j < arr.length; j++) {
+        keys[j].textContent = arr[j];
+        keys[j].onclick = (event) => {
+            const textArea = document.querySelector('.text-area');
+            textArea.focus();
+            const start = textArea.selectionStart;
+            const end = textArea.selectionEnd;
+            textArea.value = textArea.value.substring(0, start) + event.target.textContent + textArea.value.substring(end);
+            if (start === end) {
+                textArea.selectionStart = start + 1;
+                textArea.selectionEnd = end + 1;
+            }
+            else {
+                textArea.selectionStart = start + 1;
+                textArea.selectionEnd = start + 1;
+            }
+        }
     }
 
     keys[0].id = 'tilda';
     keys[13].id = 'backspace';
+    keys[13].onclick = () => {
+        const textArea = document.querySelector('.text-area');
+        textArea.focus();
+        const start = textArea.selectionStart;
+        const end = textArea.selectionEnd;
+        if (start === end) {
+            textArea.value = textArea.value.substring(0, start - 1) + textArea.value.substring(end);
+            textArea.selectionStart = start - 1;
+            textArea.selectionEnd = end - 1;
+        }
+        else {
+            textArea.value = textArea.value.substring(0, start) + textArea.value.substring(end);
+            textArea.selectionStart = start;
+            textArea.selectionEnd = start;
+        }
+    }
     keys[14].id = 'tab';
+    keys[14].onclick = () => {
+        const textArea = document.querySelector('.text-area');
+        textArea.focus();
+        const start = textArea.selectionStart;
+        const end = textArea.selectionEnd;
+        textArea.value = textArea.value.substring(0, start) + '\t' + textArea.value.substring(end);
+        if (start === end) {
+            textArea.selectionStart = start + 1;
+            textArea.selectionEnd = end + 1;
+        }
+        else {
+            textArea.selectionStart = start + 1;
+            textArea.selectionEnd = start + 1;
+        }
+    }
     keys[27].id = 'backslash';
     keys[28].id = 'caps-lock';
+    keys[28].onclick = (event) => {
+        event.target.classList.toggle('active');
+        if (event.target.classList.contains('active')) {
+            Array.from(document.querySelectorAll('.char'))
+                .map(char => char.textContent = char.textContent.toUpperCase());
+        } else {
+            Array.from(document.querySelectorAll('.char'))
+                .map(char => char.textContent = char.textContent.toLowerCase());
+        }
+    }
     keys[40].id = 'enter';
+    keys[40].onclick = () => {
+        const textArea = document.querySelector('.text-area');
+        textArea.focus();
+        const start = textArea.selectionStart;
+        const end = textArea.selectionEnd;
+        textArea.value = textArea.value.substring(0, start) + '\n' + textArea.value.substring(end);
+        if (start === end) {
+            textArea.selectionStart = start + 1;
+            textArea.selectionEnd = end + 1;
+        }
+        else {
+            textArea.selectionStart = start + 1;
+            textArea.selectionEnd = start + 1;
+        }
+    }
     keys[41].id = 'l-shift';
+    keys[41].onclick = (event) => {
+        event.target.classList.toggle('active');
+        if (event.target.classList.contains('active')) {
+            createButton(allKeysRuAltContent);
+            let lShift = document.getElementById('l-shift');
+            lShift.classList.add('active');
+            Array.from(document.querySelectorAll('.char'))
+                .map(char => char.textContent = char.textContent.toUpperCase());
+        } else {
+            createButton(allKeysRu);
+            let lShift = document.getElementById('l-shift');
+            lShift.classList.remove('active');
+            Array.from(document.querySelectorAll('.char'))
+                .map(char => char.textContent = char.textContent.toLowerCase());
+        }
+    } 
     keys[52].id = 'r-shift';
     keys[53].id = 'l-ctrl';
     keys[54].id = 'win';
     keys[55].id = 'l-alt';
     keys[56].id = 'space';
+    keys[56].onclick = () => {
+        const textArea = document.querySelector('.text-area');
+        textArea.focus();
+        const start = textArea.selectionStart;
+        const end = textArea.selectionEnd;
+        textArea.value = textArea.value.substring(0, start) + ' ' + textArea.value.substring(end);
+        if (start === end) {
+            textArea.selectionStart = start + 1;
+            textArea.selectionEnd = end + 1;
+        }
+        else {
+            textArea.selectionStart = start + 1;
+            textArea.selectionEnd = start + 1;
+        }
+    }
     keys[57].id = 'r-alt';
     keys[58].id = 'int-backslash';
     keys[59].id = 'r-ctrl';
@@ -91,7 +227,7 @@ function createButton() {
 //Events on press on physic keyboard
 
 window.addEventListener('keydown', (event) => {
-    let keys = document.querySelectorAll(".keys");
+    keys = document.querySelectorAll(".keys");
     let key = Array.from(keys).filter(key => key.textContent.replaceAll(' ', '').toUpperCase() === event.key.replaceAll(' ', '').toUpperCase())[0];
     switch (event.key) {
         case "Shift":
@@ -149,7 +285,7 @@ window.addEventListener('keydown', (event) => {
 })
 
 window.addEventListener('keyup', (event) => {
-    let keys = document.querySelectorAll(".keys");
+    keys = document.querySelectorAll(".keys");
     let key = Array.from(keys).filter(key => key.textContent.replaceAll(' ', '').toUpperCase() === event.key.replaceAll(' ', '').toUpperCase())[0];
     switch (event.key) {
         case "Shift":
@@ -205,6 +341,7 @@ window.addEventListener('keyup', (event) => {
             break;
     }
 })
+
 
 
 
